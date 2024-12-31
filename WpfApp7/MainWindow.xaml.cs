779effc7d9f6488e6d53f0a85c8a2938af56f1da
4b2g0036@office.stust.edu.tw <4b2g0036@office.stust.edu.tw>
2024-12-31 16:40:37 +0800

    更新 MainWindow 介面與資料處理邏輯
    在 MainWindow.xaml 中：
    - 移除 ContentTextBox 的 TextInput 事件處理器
    - 新增 RecordDataGrid 的 LoadingRow 事件處理器
    - 調整 StatusBarItem 寬度與 StatusTextBlock 字型大小
    
    在 MainWindow.xaml.cs 中：
    - 新增 System.Text.Json 的引用
    - 新增 AQIData、List<Field> 和 List<Record> 的實例變數
    - 更新 GetAQIButton_Click 事件處理器，反序列化資料並更新 StatusTextBlock，呼叫 DisplayAQIData 方法
    - 新增 DisplayAQIData 方法，設定 RecordDataGrid 的 ItemsSource
    - 新增 RecordDataGrid_LoadingRow 事件處理器，為每一行新增行號

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 
 namespace WpfApp7
@@ -9,7 +10,9 @@ namespace WpfApp7
     public partial class MainWindow : Window
     {
         string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
-
+        AQIData aqiData = new AQIData();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
         public MainWindow()
         {
             InitializeComponent();
@@ -21,7 +24,17 @@ namespace WpfApp7
             ContentTextBox.Text = "抓取資料中...";
             string data = await FetchContentAsync(defaultURL);
             ContentTextBox.Text = data;
-            
+            aqiData = JsonSerializer.Deserialize<AQIData>(data);
+            fields = aqiData.fields.ToList();
+            records = aqiData.records.ToList();
+
+            StatusTextBlock.Text = $"共有{records.Count}筆資料";
+            DisplayAQIData();
+        }
+
+        private void DisplayAQIData()
+        {
+            RecordDataGrid.ItemsSource = records;
         }
 
         private async Task<string> FetchContentAsync(string url)
@@ -41,6 +54,11 @@ namespace WpfApp7
                     return null;
                 }
             }
+        }
+
+        private void RecordDataGrid_LoadingRow(object sender, System.Windows.Controls.DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
         }
     }
 }
